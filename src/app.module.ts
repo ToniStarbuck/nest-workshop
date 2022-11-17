a6065b6cfe392d4bc3bb914d59f7a1157be05eca
john <toniucksstab@gmail.com>
2022-11-17 17:57:13 +0100

    database via config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CoffeesModule } from './coffees/coffees.module';
 import { RootModule } from './root/root.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     RootModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
@@ -24,4 +27,4 @@ import { DatabaseModule } from './database/database.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
